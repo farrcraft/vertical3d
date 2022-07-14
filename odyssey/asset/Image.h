d4c28ddf3deaf4c9fb78f5f0884ea2313aaa2165
Josh Farr <josh@farrcraft.com>
2022-07-13 20:04:43 -0700

    remove old dupes

--- odyssey/asset/Image.h
+++ odyssey/asset/Image.h
@@ -11,19 +11,19 @@
 #include <boost/shared_ptr.hpp>
 
 namespace odyssey::asset {
-	/**
-	 **/
-	class Image : public Asset {
-	public:
-		/**
-		 **/
-		Image(std::string_view name, Type t, boost::shared_ptr<odyssey::image::Image> img);
+    /**
+     **/
+    class Image : public Asset {
+     public:
+        /**
+         **/
+        Image(std::string_view name, Type t, boost::shared_ptr<odyssey::image::Image> img);
 
-		/**
-		 **/
-		boost::shared_ptr<odyssey::image::Image> image();
+        /**
+         **/
+        boost::shared_ptr<odyssey::image::Image> image();
 
-	private:
-		boost::shared_ptr<odyssey::image::Image> image_;
-	};
-};
+     private:
+        boost::shared_ptr<odyssey::image::Image> image_;
+    };
+};  // namespace odyssey::asset
