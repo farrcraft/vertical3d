6dcacd031e94eaf8d486a2cf4cd4f61a00413928
Josh Farr <josh@farrcraft.com>
2022-07-28 10:16:00 -0700

    whitespace

--- api/asset/Loader.cpp
+++ api/asset/Loader.cpp
@@ -1,7 +1,7 @@
 /**
  * Vertical3D
  * Copyright(c) 2022 Joshua Farr(josh@farrcraft.com)
-**/
+ **/
 
 #include "Loader.h"
 
--- api/asset/Loader.h
+++ api/asset/Loader.h
@@ -1,7 +1,7 @@
 /**
  * Vertical3D
  * Copyright(c) 2022 Joshua Farr(josh@farrcraft.com)
-**/
+ **/
 #pragma once
 
 #include "Asset.h"
