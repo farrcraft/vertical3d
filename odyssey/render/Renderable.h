a923d6a145bb33b0391be0698eea985b3a4cd77d
Joshua Farr <josh@farrcraft.com>
2023-09-03 09:17:59 -0700

    Use API's render engine

--- odyssey/render/Renderable.h
+++ odyssey/render/Renderable.h
@@ -1,32 +1,28 @@
 /**
  * The Untitled Adventure / Odyssey
- * Copyright (c) 2021 Joshua Farr (josh@farrcraft.com)
+ * Copyright (c) 2023 Joshua Farr (josh@farrcraft.com)
  **/
 
 #pragma once
 
+#include "../../../api/render/realtime/2D/Engine2D.h"
 #include "../../api/render/realtime/Frame.h"
 
 namespace odyssey::render {
-    /**
-     * Forward declaration 
-     **/
-    class Engine;
-
     /**
      **/
     class Renderable {
     public:
         /**
          **/
-        Renderable(boost::shared_ptr<Engine> renderer);
+        Renderable(boost::shared_ptr<v3d::render::realtime::Engine2D> renderer);
 
         /**
          **/
         virtual void draw(boost::shared_ptr<v3d::render::realtime::Frame> frame) = 0;
 
     protected:
-        boost::shared_ptr<odyssey::render::Engine> renderer_;
+        boost::shared_ptr<v3d::render::realtime::Engine2D> renderer_;
     };
 
 };  // namespace odyssey::render
--- odyssey/render/renderable/Player.cpp
+++ odyssey/render/renderable/Player.cpp
@@ -1,6 +1,6 @@
 /**
  * The Untitled Adventure / Odyssey
- * Copyright (c) 2021 Joshua Farr (josh@farrcraft.com)
+ * Copyright (c) 2023 Joshua Farr (josh@farrcraft.com)
  **/
 
 #include "Player.h"
@@ -13,39 +13,40 @@
 
 #include <boost/make_shared.hpp>
 
-using namespace odyssey::render;
-using namespace odyssey::render::renderable;
+namespace odyssey::render::renderable {
 
-/**
- **/
-Player::Player(boost::shared_ptr<Engine> renderer, boost::shared_ptr<odyssey::engine::Player> player) :
-    Renderable(renderer),
-    player_(player) {
+    /**
+     **/
+    Player::Player(boost::shared_ptr<v3d::render::realtime::Engine2D> renderer, boost::shared_ptr<odyssey::engine::Player> player) :
+        Renderable(renderer),
+        player_(player) {
 
-    //boost::shared_ptr<odyssey::asset::Image> asset = assetManager->load("sample.png", odyssey::asset::Type::IMAGE_PNG);
-    boost::shared_ptr<v3d::asset::Asset> asset = renderer_->assetManager()->loadTypeFromExt("sample.png");
-    boost::shared_ptr<v3d::asset::Image> img = boost::static_pointer_cast<v3d::asset::Image>(asset);
+        //boost::shared_ptr<odyssey::asset::Image> asset = assetManager->load("sample.png", odyssey::asset::Type::IMAGE_PNG);
+        boost::shared_ptr<v3d::asset::Asset> asset = renderer_->assetManager()->loadTypeFromExt("sample.png");
+        boost::shared_ptr<v3d::asset::Image> img = boost::static_pointer_cast<v3d::asset::Image>(asset);
 
-    // create surface from image
-    boost::shared_ptr<v3d::render::realtime::Surface> surface = boost::make_shared<v3d::render::realtime::Surface>(img->image());
+        // create surface from image
+        boost::shared_ptr<v3d::render::realtime::Surface> surface = boost::make_shared<v3d::render::realtime::Surface>(img->image());
 
-    // create texture from surface
-    boost::shared_ptr<v3d::render::realtime::Texture2D> texture = boost::make_shared<v3d::render::realtime::Texture2D>(renderer_->context(), surface->surface(), img->image()->width(), img->image()->height());
+        // create texture from surface
+        boost::shared_ptr<v3d::render::realtime::Texture2D> texture = boost::make_shared<v3d::render::realtime::Texture2D>(renderer_->context(), surface->surface(), img->image()->width(), img->image()->height());
 
-    // we need to scale our texture to our tile size proportions
-    texture->resize(odyssey::engine::unit::tile_width, odyssey::engine::unit::tile_height);
+        // we need to scale our texture to our tile size proportions
+        texture->resize(odyssey::engine::unit::tile_width, odyssey::engine::unit::tile_height);
 
-    // put the texture into the cache
-    renderer_->textureCache()->cache("sample.png", texture);
-}
+        // put the texture into the cache
+        renderer_->textureCache()->cache("sample.png", texture);
+    }
 
-/**
- **/
-void Player::draw(boost::shared_ptr<v3d::render::realtime::Frame> frame) {
-    // fetch our source texture from the cache
-    boost::shared_ptr<v3d::render::realtime::Texture2D> source = renderer_->textureCache()->fetch("sample.png");
-    // create a new blit texture render operation
-    boost::shared_ptr<v3d::render::realtime::operation::Blit2DTexture> operation = boost::make_shared<v3d::render::realtime::operation::Blit2DTexture>(source, renderer_->backBuffer());
-    // push onto frame
-    frame->addOperation(operation);
-}
+    /**
+     **/
+    void Player::draw(boost::shared_ptr<v3d::render::realtime::Frame> frame) {
+        // fetch our source texture from the cache
+        boost::shared_ptr<v3d::render::realtime::Texture2D> source = renderer_->textureCache()->fetch("sample.png");
+        // create a new blit texture render operation
+        boost::shared_ptr<v3d::render::realtime::operation::Blit2DTexture> operation = boost::make_shared<v3d::render::realtime::operation::Blit2DTexture>(source, renderer_->backBuffer());
+        // push onto frame
+        frame->addOperation(operation);
+    }
+
+};  // namespace odyssey::render::renderable
