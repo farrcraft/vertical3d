457ad92b5466ae8ee9689d0d9849f0ca4ac0f4ab
Joshua Farr <josh@farrcraft.com>
2023-09-10 10:30:05 -0700

    Move dag into api

--- api/dag/Edge.h
+++ api/dag/Edge.h
@@ -1,6 +1,6 @@
 /**
  * Vertical3D
- * Copyright(c) 2022 Joshua Farr(josh@farrcraft.com)
+ * Copyright(c) 2023 Joshua Farr(josh@farrcraft.com)
 **/
 
 #pragma once
@@ -11,4 +11,4 @@ namespace v3d::dag {
         Edge();
         virtual ~Edge();
     };
-};  // end namesapce v3D
+};  // end namespace v3d::dag
