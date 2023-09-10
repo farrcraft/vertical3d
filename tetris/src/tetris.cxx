3edbc19f9735d28e19de743f0704c1ca0aead820
Joshua Farr <josh@farrcraft.com>
2023-09-10 10:42:17 -0700

    standard app entrypoints

--- tetris/src/tetris.cxx
+++ tetris/src/tetris.cxx
@@ -3,15 +3,26 @@
  * Copyright(c) 2022 Joshua Farr(josh@farrcraft.com)
  **/
 
-#include <cstdlib>
-
 #include "Controller.h"
 
-int main(int argc, char *argv[]) {
-    Controller controller;
+#include <cstdlib>
 
-    if (!controller.run()) {
-        return EXIT_FAILURE;
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+
+int main(int argc, char *argv[]) {
+    // extract exe path from argv (needed for loading file assets with relative paths)
+    std::string appPath =
+        boost::filesystem::path(boost::filesystem::system_complete(boost::filesystem::path(argv[0])).remove_filename()).string() +
+        boost::filesystem::path("/").make_preferred().string();
+
+    Controller controller(appPath);
+
+    int exitStatus = EXIT_SUCCESS;
+    if (!controller.initialize() || !controller.eventLoop()) {
+        exitStatus = EXIT_FAILURE;
     }
-    return EXIT_SUCCESS;
+    if (!controller.shutdown()) {
+        exitStatus = EXIT_FAILURE;
+    }
 }
