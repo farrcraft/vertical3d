8ba25083d571c4d97de3c5d17bdc7bc3012106c5
Joshua Farr <josh@farrcraft.com>
2023-09-16 10:04:32 -0700

    formatting

--- api/render/realtime/2D/Engine2D.h
+++ api/render/realtime/2D/Engine2D.h
@@ -17,22 +17,22 @@
 
 namespace v3d::render::realtime {
     /* The render engine.
-    * This is different from the game engine.
-    * While the game engine is responsible for coordinating the game,
-    * it is the responsibility of the render engine to manage the rendering pipeline.
+     * This is different from the game engine.
+     * While the game engine is responsible for coordinating the game,
+     * it is the responsibility of the render engine to manage the rendering pipeline.
     **/
     class Engine2D : public Engine {
      public:
         /**
-            **/
+         **/
         Engine2D(const boost::shared_ptr<v3d::log::Logger>& logger, const boost::shared_ptr<v3d::asset::Manager>& assetManager, entt::registry* registry);
 
         /**
-            **/
+         **/
         bool initialize(const boost::shared_ptr<Window2D>& window);
 
         /**
-            **/
+         **/
         boost::shared_ptr<Texture2DCache> textureCache();
 
         /**
@@ -40,15 +40,15 @@ namespace v3d::render::realtime {
         void renderFrame();
 
         /**
-            **/
+         **/
         boost::shared_ptr<Context2D> context();
 
         /**
-            **/
+         **/
         boost::shared_ptr<Texture2D> backBuffer();
 
         /**
-            **/
+         **/
         boost::shared_ptr<Scene2D> scene();
 
      private:
